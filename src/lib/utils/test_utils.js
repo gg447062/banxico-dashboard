4696127d40cf58f18c7557b94158139baa64cb93
Gabriel Gutierrez <gabrielgutierrez@Gabriels-MacBook-Pro.local>
2023-03-08 15:48:56 -0600

    adds initialState with entities included for testing

--- src/lib/utils/test_utils.js
+++ src/lib/utils/test_utils.js
@@ -111,3 +111,67 @@ export const expectedTableResult = {
     colors: {},
   },
 };
+
+export const initialStateWithVisualizations = {
+  visualizations: {
+    status: 'idle',
+    entities: {
+      1234: {
+        data: [
+          { datos: ['3876663.0'], fecha: '12/30/2022' },
+          { datos: ['3829690.1'], fecha: '01/06/2023' },
+          { datos: ['3770091.8'], fecha: '01/13/2023' },
+          { datos: ['3801964.3'], fecha: '01/20/2023' },
+          { datos: ['3776123.6'], fecha: '01/27/2023' },
+          { datos: ['3802009.1'], fecha: '02/03/2023' },
+          { datos: ['3750742.6'], fecha: '02/10/2023' },
+          { datos: ['3689728.7'], fecha: '02/17/2023' },
+          { datos: ['3681990.5'], fecha: '02/24/2023' },
+          { datos: ['3607278.8'], fecha: '03/03/2023' },
+        ],
+        titlesList: [
+          'Principales renglones del estado de cuenta del Banco de México.- Activo Reserva Internacional',
+        ],
+        selectedSeries: ['SF43658'],
+        startDate: '2023-01-02',
+        endDate: '2023-03-08',
+        language: 'es',
+        type: 'table',
+        title: 'test-table',
+        id: '1234',
+        dateFormat: 'mm/dd/yyyy',
+        decimals: '1',
+        graphType: '',
+        colors: {},
+      },
+      5678: {
+        data: [
+          { SF43658: 3876663.0, fecha: '30/12/2022' },
+          { SF43658: 3829690.1, fecha: '06/01/2023' },
+          { SF43658: 3770091.8, fecha: '13/01/2023' },
+          { SF43658: 3801964.3, fecha: '20/01/2023' },
+          { SF43658: 3776123.6, fecha: '27/01/2023' },
+          { SF43658: 3802009.1, fecha: '03/02/2023' },
+          { SF43658: 3750742.6, fecha: '10/02/2023' },
+          { SF43658: 3689728.7, fecha: '17/02/2023' },
+          { SF43658: 3681990.5, fecha: '24/02/2023' },
+          { SF43658: 3607278.8, fecha: '03/03/2023' },
+        ],
+        titlesList: [
+          'Principales renglones del estado de cuenta del Banco de México.- Activo Reserva Internacional',
+        ],
+        selectedSeries: ['SF43658'],
+        startDate: '2023-01-01',
+        endDate: '2023-03-08',
+        language: 'es',
+        type: 'graph',
+        title: 'test-graph',
+        id: '5678',
+        dateFormat: '',
+        decimals: '',
+        graphType: 'bar',
+        colors: { SF43658: 'pink' },
+      },
+    },
+  },
+};
